f4fd1f5a3c5d23965b7437e1464a00466dad76d7
umesh bisht1 <umeshbisht792@gmail.com>
2023-09-16 15:51:43 +0530

    designed the css

--- public/index.js
+++ public/index.js
@@ -3,10 +3,7 @@ document.getElementsByClassName("hover")[0].addEventListener("mouseenter", () =>
    document.getElementById("catch").classList.add("some");
 })
 
-// document.getElementsByClassName("hover")[0].addEventListener("dbclick", () => {
-//     document.getElementById("catch").classList.add("list");
-//     document.getElementById("catch").classList.remove("some");
-//  })
+
  
  document.getElementById("catch").addEventListener("mouseenter", () => {
     document.getElementById("catch").classList.remove("list");
@@ -17,9 +14,7 @@ document.getElementsByClassName("hover")[0].addEventListener("mouseenter", () =>
     document.getElementById("catch").classList.add("list");
    document.getElementById("catch").classList.remove("some");
  })
-//  document.getElementById("tick").addEventListener("click",()=>{
-//    document.getElementById("menu").style.opacity="1";
-//  })
+
   document.getElementById("trueorfalse").addEventListener("click",()=>{
    var y=document.getElementById("trueorfalse").checked;
    if(y==1)
